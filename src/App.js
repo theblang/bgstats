df18a7111ab1db7cbb13822dac5b55ed52a0db6d
Matt Langston <theblang@gmail.com>
2019-03-26 20:32:00 -0500

    Convert App to a function component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import {
@@ -13,9 +13,10 @@ import {
     IconButton,
     Typography,
     Hidden,
-    Drawer,
-    withStyles
+    Drawer
 } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import SettingsIcon from '@material-ui/icons/Settings';
 import CollectionsIcon from '@material-ui/icons/Collections';
@@ -26,151 +27,153 @@ import Settings from './Settings';
 import Dashboard from './Dashboard';
 import Title from './Title';
 
-const drawerWidth = 240;
-
-const styles = theme => ({
-    root: {
-        display: 'flex'
-    },
-    drawer: {
-        [theme.breakpoints.up('sm')]: {
-            width: drawerWidth,
-            flexShrink: 0
-        }
-    },
-    appBar: {
-        marginLeft: drawerWidth,
-        [theme.breakpoints.up('sm')]: {
-            width: `calc(100% - ${drawerWidth}px)`
-        }
-    },
-    menuButton: {
-        marginRight: 20,
-        [theme.breakpoints.up('sm')]: {
-            display: 'none'
-        }
-    },
-    toolbar: theme.mixins.toolbar,
-    drawerPaper: {
-        width: drawerWidth
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing.unit * 3
+const theme = createMuiTheme({
+    typography: {
+        useNextVariants: true
     }
 });
+const drawerWidth = 240;
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            mobileOpen: false
+const useStyles = makeStyles(
+    theme => {
+        return {
+            root: {
+                display: 'flex'
+            },
+            drawer: {
+                [theme.breakpoints.up('sm')]: {
+                    width: drawerWidth,
+                    flexShrink: 0
+                }
+            },
+            appBar: {
+                marginLeft: drawerWidth,
+                [theme.breakpoints.up('sm')]: {
+                    width: `calc(100% - ${drawerWidth}px)`
+                }
+            },
+            menuButton: {
+                marginRight: 20,
+                [theme.breakpoints.up('sm')]: {
+                    display: 'none'
+                }
+            },
+            toolbar: theme.mixins.toolbar,
+            drawerPaper: {
+                width: drawerWidth
+            },
+            content: {
+                flexGrow: 1,
+                padding: theme.spacing.unit * 3
+            }
         };
+    },
+    {
+        defaultTheme: theme
     }
+);
 
-    handleDrawerToggle = () => {
-        this.setState(state => ({ mobileOpen: !state.mobileOpen }));
-    };
+export default function App(props) {
+    const classes = useStyles();
+    const [mobileOpen, setMobileOpen] = useState(false);
 
-    render() {
-        const { classes, theme } = this.props;
+    const drawer = (
+        <div>
+            <div className={classes.toolbar} />
+            <Divider />
+            <List>
+                <Link to="/collection" style={{ textDecoration: 'none' }}>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <CollectionsIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Collection" />
+                    </ListItem>
+                </Link>
+                <Link to="/sessions" style={{ textDecoration: 'none' }}>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <ScoreIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Sessions" />
+                    </ListItem>
+                </Link>
+            </List>
+            <Divider />
+            <List>
+                <Link to="/settings" style={{ textDecoration: 'none' }}>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <SettingsIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Settings" />
+                    </ListItem>
+                </Link>
+            </List>
+        </div>
+    );
 
-        const drawer = (
-            <div>
-                <div className={classes.toolbar} />
-                <Divider />
-                <List>
-                    <Link to="/collection" style={{ textDecoration: 'none' }}>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <CollectionsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Collection" />
-                        </ListItem>
-                    </Link>
-                    <Link to="/sessions" style={{ textDecoration: 'none' }}>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <ScoreIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Sessions" />
-                        </ListItem>
-                    </Link>
-                </List>
-                <Divider />
-                <List>
-                    <Link to="/settings" style={{ textDecoration: 'none' }}>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <SettingsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Settings" />
-                        </ListItem>
-                    </Link>
-                </List>
+    return (
+        <Router>
+            <div className={classes.root}>
+                <CssBaseline />
+                <AppBar position="fixed" className={classes.appBar}>
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="Open drawer"
+                            onClick={() => {
+                                setMobileOpen(!mobileOpen);
+                            }}
+                            className={classes.menuButton}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" noWrap>
+                            <Title />
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <nav className={classes.drawer}>
+                    {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                    <Hidden smUp implementation="css">
+                        <Drawer
+                            container={props.container}
+                            variant="temporary"
+                            anchor={
+                                theme.direction === 'rtl' ? 'right' : 'left'
+                            }
+                            open={mobileOpen}
+                            onClose={() => {
+                                setMobileOpen(!mobileOpen);
+                            }}
+                            classes={{
+                                paper: classes.drawerPaper
+                            }}
+                        >
+                            {drawer}
+                        </Drawer>
+                    </Hidden>
+                    <Hidden xsDown implementation="css">
+                        <Drawer
+                            classes={{
+                                paper: classes.drawerPaper
+                            }}
+                            variant="permanent"
+                            open
+                        >
+                            {drawer}
+                        </Drawer>
+                    </Hidden>
+                </nav>
+                <main className={classes.content}>
+                    <div className={classes.toolbar} />
+                    <Route exact path="/" component={Dashboard} />
+                    <Route path="/collection" component={Collection} />
+                    <Route path="/sessions" component={Sessions} />
+                    <Route path="/settings" component={Settings} />
+                </main>
             </div>
-        );
-
-        return (
-            <Router>
-                <div className={classes.root}>
-                    <CssBaseline />
-                    <AppBar position="fixed" className={classes.appBar}>
-                        <Toolbar>
-                            <IconButton
-                                color="inherit"
-                                aria-label="Open drawer"
-                                onClick={this.handleDrawerToggle}
-                                className={classes.menuButton}
-                            >
-                                <MenuIcon />
-                            </IconButton>
-                            <Typography variant="h6" color="inherit" noWrap>
-                                <Title location={{}} />
-                            </Typography>
-                        </Toolbar>
-                    </AppBar>
-                    <nav className={classes.drawer}>
-                        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-                        <Hidden smUp implementation="css">
-                            <Drawer
-                                container={this.props.container}
-                                variant="temporary"
-                                anchor={
-                                    theme.direction === 'rtl' ? 'right' : 'left'
-                                }
-                                open={this.state.mobileOpen}
-                                onClose={this.handleDrawerToggle}
-                                classes={{
-                                    paper: classes.drawerPaper
-                                }}
-                            >
-                                {drawer}
-                            </Drawer>
-                        </Hidden>
-                        <Hidden xsDown implementation="css">
-                            <Drawer
-                                classes={{
-                                    paper: classes.drawerPaper
-                                }}
-                                variant="permanent"
-                                open
-                            >
-                                {drawer}
-                            </Drawer>
-                        </Hidden>
-                    </nav>
-                    <main className={classes.content}>
-                        <div className={classes.toolbar} />
-                        <Route exact path="/" component={Dashboard} />
-                        <Route path="/collection" component={Collection} />
-                        <Route path="/sessions" component={Sessions} />
-                        <Route path="/settings" component={Settings} />
-                    </main>
-                </div>
-            </Router>
-        );
-    }
+        </Router>
+    );
 }
-
-export default withStyles(styles, { withTheme: true })(App);
